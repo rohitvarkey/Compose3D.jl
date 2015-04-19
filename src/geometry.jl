d56d880a7da4d00b92bddde24a863be418d813e5
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2015-04-19 10:11:56 +0530

    Cube function and Cube Geometry added

--- src/geometry.jl
+++ src/geometry.jl
@@ -5,10 +5,17 @@ immutable Geometry{P <: GeometryPrimitive} <: Compose3DNode
 	primitives::Vector{P}
 end
 
-#Cube primitive
+#Cube
+
 immutable CubePrimitive <: GeometryPrimitive
 	corner::Point{3}
 	side::Length{:mm}
 end
 
+typealias Cube Geometry{CubePrimitive}
+
 CubePrimitive(x::Length,y::Length,z::Length,side::Length) = CubePrimitive(Point(x,y,z),side)
+
+function cube(x::Length,y::Length,z::Length,side::Length)
+	return Cube([CubePrimitive(x::Length,y::Length,z::Length,side::Length)])
+end

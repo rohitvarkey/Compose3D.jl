a29e7d845c3d89a2e0d188274f385652944f2b91
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2015-07-22 18:08:15 +0530

    Removed loading old JS code. Does the three-js html import in an IJulia session

--- src/Compose3D.jl
+++ src/Compose3D.jl
@@ -1,5 +1,5 @@
 module Compose3D
-
+using Compat
 # Defining compose node type
 abstract Compose3DNode
 
@@ -21,23 +21,17 @@ include("webgl.jl")
 include("patchwork.jl")
 
 #Setting up three js files required to display stuff.
-#TODO: Do this only if the MIME is "text/html"
-
-asset(url) = readall(Pkg.dir("Compose3D", "src", "backends", "WebGL", "js", url))
-
-threejs = asset("three.min.js")
-mainjs = asset("main.js")
-geometryjs = asset("geometry.js")
-trackballjs = asset("trackball.js")
-
-#display(MIME"text/html"(),"<script>$threejs</script>")
-#display(MIME"text/html"(),"<script>$mainjs</script>")
-#display(MIME"text/html"(),"<script>$geometryjs</script>")
-#display(MIME"text/html"(),"<script>$trackballjs</script>")
-display(MIME"text/html"(),"<link rel=import href=/static/components/compose3d/bower_components/three-js/three-js.html>")
+#Link to the static files copied to the julia profile if in an IJulia session.
+if isdefined(Main, :IJulia)
+    display(
+        MIME"text/html"(),
+        "<link rel=import href=/static/components/compose3d/bower_components/three-js/three-js.html>"
+    )
+end
+#Assuming that Escher is taking care of the other cases.
 
 function writemime(io::IO,mime::MIME{symbol("text/html")},ctx::Context)
-	backend = Elem(:div, style=Dict(:width=>"100%", :height=>"600px")) << draw(Patchable3D(100,100),ctx)
+	backend = Elem(:div, style=@compat Dict(:width=>"100%", :height=>"600px")) << draw(Patchable3D(100,100),ctx)
 	display(backend)
 end
 
